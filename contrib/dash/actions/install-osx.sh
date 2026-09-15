29edcfa24f5eb590012ad19f376b803f5b7cb911
levoncrypto <levoncrypto1994@gmail.com>
2026-09-15 20:23:40 +0400

    Macos build fix

--- contrib/dash/actions/install-osx.sh
+++ contrib/dash/actions/install-osx.sh
@@ -43,8 +43,11 @@ rm -rf libsecp256k1/ ${LSECP256K1_FILE} ${LSECP256K1_FILE}.sha256
 if [[ "$(uname -m)" == "arm64" ]]; then
     softwareupdate --install-rosetta --agree-to-license || true
     if [[ ! -x /usr/local/bin/brew ]]; then
-        arch -x86_64 /bin/bash -c \
-            "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+        sudo mkdir -p /usr/local/Homebrew
+        sudo chown -R "$(whoami)" /usr/local/Homebrew
+        git clone --depth 1 https://github.com/Homebrew/brew /usr/local/Homebrew
+        sudo mkdir -p /usr/local/bin
+        sudo ln -sf /usr/local/Homebrew/bin/brew /usr/local/bin/brew
     fi
     arch -x86_64 /usr/local/bin/brew install gettext libtool automake pkg-config
 else
